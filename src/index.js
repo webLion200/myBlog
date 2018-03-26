5588eff5ed0a7b70640b3b73f05f4a20d6114d4a
webLion200 <794358525@qq.com>
2018-03-26 19:00:08 +0800

    请求成功

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+
 import 'antd/dist/antd.css';
 import './index.css';
 import App from './component/App'
+import reducer from './reducer'
+const store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension?window.devToolsExtension():f=>f
+))
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
-
