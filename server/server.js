a59d6d27ae77220e11647ef682cd88906cc461aa
webLion200 <794358525@qq.com>
2018-03-27 20:15:22 +0800

    分页

--- server/server.js
+++ server/server.js
@@ -6,15 +6,27 @@ var http = require('http');
 
 app.get('/blog', function(req, res) {
     Blog.find({}, function(err, doc) {
-        res.json(doc)
+        // res.json(doc)
+
+        var data = [];
+        var page = parseInt(req.query.page) || 1;
+        for(var i = 10 * (page-1); i < 10 * page; i++ ) {
+            if(!doc[i]) {
+                break;
+            }
+            data.push(doc[i]);
+        }
+        totalCount = doc.length;
+        res.send({data,totalCount, page});
     })
 })
 
 app.get('/data', function(req,res){
     // 获取数据
-    // Blog.find({}, function(err, doc) {
-    //     res.json(doc)
-    // })
+    Blog.find({}, function(err, doc) {
+        res.json(doc)
+
+    });
 
     // Blog.find({user:'hua'}, function(err, doc) {
     //     res.json(doc)
